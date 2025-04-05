063f11a2a28734abdcfa14a998a9d224309ffb83
Luca Rizzo <lucalucasrizzo@gmail.com>
2025-04-05 13:28:12 +0200

    correzione script

--- assignment2/run_benchmark.sh
+++ assignment2/run_benchmark.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-RANGE_VALUES="1-1000 5000-100000 1000000-5000000"
+RANGE_VALUES="1-1000 50000000-100000000 1000000000-1100000000"
 out_dir="./out"
 scheduling_policy=("d" "s" "t")
-chunk_size=("1" "2" "8" "16" "32" "128" "512" "1024" "5000" "10000" "50000")
-num_thread=("4" "8" "16" "32" "40")
-NUM_RUNS=3
-CSV_FILE="./out/results2.csv"
+chunk_size=("8" "32" "512" "1024" "5000" "10000")
+num_thread=("4" "16" "32" "40")
+NUM_RUNS=2
+CSV_FILE="./out/results.csv"
 
 mkdir -p "$out_dir"
 
 # Header CSV
-echo "target,policy,chunk_size,num_threads,ranges, run1,run2,run3" > "$CSV_FILE"
+echo "target,policy,chunk_size,num_threads,ranges, run1,run2" > "$CSV_FILE"
 
 for policy in "${scheduling_policy[@]}"; do
   for c in "${chunk_size[@]}"; do
@@ -19,7 +19,7 @@ for policy in "${scheduling_policy[@]}"; do
         csv_line="collatz_par, $policy,$c,$n,$RANGE_VALUES"
         echo "Running with -$policy -c $c -n $n $RANGE_VALUES"
         for ((i = 1; i <= NUM_RUNS; i++)); do
-          output=$(./collatz_par -$policy -c "$c" -n "$n" $RANGE_VALUES 2>/dev/null)
+          output=$(./collatz_par -$policy -c $c -n $n $RANGE_VALUES 2>/dev/null)
           current_run_time=$(echo "$output" | sed 's/.*: \(.*\)s/\1/')
           csv_line="$csv_line,$current_run_time"
         done
