8a32202e3ee0d2fc37fd81027b8e4a6ba70d8aa2
Luca Rizzo <lucalucasrizzo@gmail.com>
2025-04-27 12:29:18 +0200

    benchmark e success generico

--- assignment3/par_ver/minizpar.cpp
+++ assignment3/par_ver/minizpar.cpp
@@ -15,16 +15,24 @@
 namespace fs = std::filesystem;
 using namespace std;
 
-void execute_op(const string &file_name, const CompressionParams &cpar) {
-    bool success = cpar.op_to_perform == COMPRESS ?
-        block_compress(file_name, cpar) :
-        block_decompress(file_name, cpar);
-    if (success && cpar.remove_origin) {
-        unlink(file_name.c_str());
+void execute_op(const string &filename, const CompressionParams &cpar, atomic_bool &global_success) {
+    if (cpar.op_to_perform == DECOMPRESS && !filename.ends_with(".zip")) {
+        log_msg(VERBOSE, cpar, "%s is not a zip: the file will not be decompressed\n", filename.c_str());
+        return;
+    }
+    bool local_success = cpar.op_to_perform == COMPRESS
+                             ? block_compress(filename, cpar)
+                             : block_decompress(filename, cpar);
+    if (!local_success) {
+        global_success.store(false);
+        return;
+    }
+    if (cpar.remove_origin) {
+        unlink(filename.c_str());
     }
 }
 
-void traverse_and_apply_op(const string &dir_to_traverse, const CompressionParams &cpar) {
+void traverse_and_apply_op(const string &dir_to_traverse, const CompressionParams &cpar, atomic_bool &global_success) {
     std::vector<fs::directory_entry> snapshot;
     for (const auto &entry: fs::directory_iterator(dir_to_traverse)) {
         snapshot.push_back(entry);
@@ -32,33 +40,33 @@ void traverse_and_apply_op(const string &dir_to_traverse, const CompressionParam
     for (const auto &entry: snapshot) {
         auto file_name = entry.path().generic_string();
         if (entry.is_directory() && cpar.scan_subdirectories) {
-#pragma omp task
+#pragma omp task shared(global_success)
             {
-                traverse_and_apply_op(file_name, cpar);
+                traverse_and_apply_op(file_name, cpar, global_success);
             }
         } else if (entry.is_regular_file()) {
-#pragma omp task
+#pragma omp task shared(global_success)
             {
-                execute_op(file_name, cpar);
+                execute_op(file_name, cpar, global_success);
             }
         }
     }
 }
 
-void do_work(const string &file_name, const CompressionParams &cpar) {
+void do_work(const string &file_name, const CompressionParams &cpar, atomic_bool &global_success) {
     if (fs::is_directory(file_name)) {
-        traverse_and_apply_op(file_name, cpar);
+        traverse_and_apply_op(file_name, cpar, global_success);
     } else {
-#pragma omp task
+#pragma omp task shared(global_success)
         {
-            execute_op(file_name, cpar);
+            execute_op(file_name, cpar, global_success);
         }
     }
 }
 
 int main(int argc, char *argv[]) {
     CompressionParams cpar = parseCommandLine(argc, argv);
-    
+    atomic_bool global_success(true);
     TIMERSTART(minizpar)
 #pragma omp parallel
     {
@@ -66,11 +74,17 @@ int main(int argc, char *argv[]) {
         {
             for (const std::string &file: cpar.files) {
                 {
-                    do_work(file, cpar);
+                    do_work(file, cpar, global_success);
                 }
             }
         }
 #pragma omp taskwait
     }
     TIMERSTOP(minizpar)
+    if (!global_success.load()) {
+        printf("Exiting with Failure\n");
+        return -1;
+    }
+    printf("Exiting with Success\n");
+    return 0;
 }
--- assignment3/run_benchmark.sh
+++ assignment3/run_benchmark.sh
@@ -1,14 +1,52 @@
 #!/bin/bash
 
+OUT_DIR="./out"
+CSV_FILE="$OUT_DIR/$1.csv"
+NUM_RUNS=3
+TEST_DIR="./test_file"
+THREADS_LIST=(2 4 8 16 32 40)
+
+mkdir -p "$OUT_DIR"
+
+echo "Compiling all sources..."
 make compile_all
 make clean_test_file
 
 ./create_test_file.sh
-OMP_NUM_THREADS=32 ./minizpar -r 1 -C 0 ./test_file/many_small_files
-OMP_NUM_THREADS=32 ./minizpar -r 1 -C 0 ./test_file/few_big_file
 
-make clean_compression_test_file
+echo "target,num_threads,dataset,run1,run2,run3" > "$CSV_FILE"
 
-./minizseq -r 1 -C 0 ./test_file/many_small_files
-./minizseq -r 1 -C 0 ./test_file/few_big_file
+# Helper per eseguire e raccogliere i tempi
+run_and_measure() {
+  local target="$1"
+  local num_threads="$2"
+  local dataset="$3"
+  local cmd="$4"
+
+  local csv_line="$target,$num_threads,$dataset"
+  echo "Running $cmd"
+  for ((i = 1; i <= NUM_RUNS; i++)); do
+    make clean_compression_test_file > /dev/null 2>&1
+    output=$(eval "$cmd")
+    current_run_time=$(echo "$output" | grep -i "elapsed time" | sed 's/.*: \(.*\)s/\1/')
+    csv_line="$csv_line,$current_run_time"
+  done
+  echo "$csv_line" >> "$CSV_FILE"
+}
+
+for n_threads in "${THREADS_LIST[@]}"; do
+  export OMP_NUM_THREADS="$n_threads"
+  for dataset in large small mixed; do
+    run_and_measure "minizpar" "$n_threads" "$dataset" "./minizpar -r 1 -C 0 $TEST_DIR/$dataset"
+  done
+  unset OMP_NUM_THREADS
+done
+
+for dataset in large small mixed; do
+  run_and_measure "minizseq" "1" "$dataset" "./minizseq -r 1 -C 0 $TEST_DIR/$dataset"
+done
+
+echo "Cleaning all test files..."
 make clean_test_file
+
+echo "All benchmarks completed. Results saved in $CSV_FILE"
